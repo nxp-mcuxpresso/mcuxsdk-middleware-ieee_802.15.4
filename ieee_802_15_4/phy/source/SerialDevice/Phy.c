8772a254cae72376dd94abe1d626a3f793e4cf51
George Stefan <george.stefan@nxp.com>
2024-11-12 13:23:07 +0200

    [MCZB-2530] k32w1: split_PHY: interface: fixed deadlock/multitasking scenarios
    - added mutex for multitasking protection
    - added PHY indication processing task to avoid deadlocking in PhyRpmsgRxCallback(IMU ISR)
    - added flag for get PIB to avoid spurious events
    - handle error cases
    - PHY_get_ctx() waits for NBU to be ready
    
    Signed-off-by: George Stefan <george.stefan@nxp.com>

--- ieee_802_15_4/phy/source/SerialDevice/Phy.c
+++ ieee_802_15_4/phy/source/SerialDevice/Phy.c
@@ -24,7 +24,13 @@
 #undef CTX_NO
 #define CTX_NO 2
 
-struct phy_local
+#define PHY_GET_CTX_CNT 10
+
+#define PHY_INTF_CB_QUEUE_SIZE 10
+#define PHY_INTF_CB_TASK_PRIORITY 4
+#define PHY_INTF_CB_TASK_STACK_SIZE 1024
+
+static struct phy_local
 {
     uint32_t id;
 
@@ -40,14 +46,125 @@ static hal_rpmsg_config_t phyRpmsgConfig = {
     .imuLink     = kIMU_LinkCpu2Cpu3
 #endif
 };
-messaging_t plmeMsgQueue;
-OSA_EVENT_HANDLE_DEFINE(plmeEventHandle);
+
+static uint8_t wait_phy_rsp = FALSE;
+static plmeGetReq_t phy_get_rsp;
+
+static OSA_EVENT_HANDLE_DEFINE(get_event);
+
+static OSA_MUTEX_HANDLE_DEFINE(phy_intf_mutex);
+static OSA_MSGQ_HANDLE_DEFINE(phy_intf_cb_queue, PHY_INTF_CB_QUEUE_SIZE, sizeof(phyMessageHeader_t *));
+
+static void phy_intf_cb_task_func();
+static OSA_TASK_HANDLE_DEFINE(phy_intf_cb_task);
+static OSA_TASK_DEFINE(phy_intf_cb_task_func, PHY_INTF_CB_TASK_PRIORITY, 1, PHY_INTF_CB_TASK_STACK_SIZE, FALSE);
+
+static bool_t phy_intf_init_done = FALSE;
+static bool_t phy_intf_init_ongoing = FALSE;
+extern const uint8_t gUseRtos_c;
+
+
+static void wait_response()
+{
+    osa_event_flags_t flags;
+    uint32_t dt = osaWaitForever_c;
+
+    if (!OSA_TaskGetCurrentHandle())
+    {
+        /* Current task is valid in OSA_ProcessTasks().
+           There is no check in OSA_EventWait() that current task is valid.
+           For osaWaitNone_c, current task is not used */
+        dt = osaWaitNone_c;
+    }
+
+    /* Wait until NBU delivers result over RPMSG */
+    while (OSA_EventWait(get_event, 1, 1, dt, &flags) != KOSA_StatusSuccess)
+    {}
+
+    /* Clear event as auto clear is not enabled */
+    OSA_EventClear(get_event, 1);
+}
+
+static void phy_intf_cb_task_func()
+{
+    phyMessageHeader_t *msg;
+    bool_t msg_free;
+
+    while (1)
+    {
+        if (OSA_MsgQGet(phy_intf_cb_queue, &msg, osaWaitForever_c) == KOSA_StatusSuccess)
+        {
+            msg_free = TRUE;
+
+            switch (msg->msgType)
+            {
+                case gPdDataInd_c:
+                    if (phyLocal[msg->ctx_id].PD_MAC_SapHandler)
+                    {
+                        msg_free = FALSE;
+                        ((pdDataToMacMessage_t *)msg)->msgData.dataInd.pPsdu = (uint8_t *)msg + sizeof(pdDataToMacMessage_t);
+
+                        phyLocal[msg->ctx_id].PD_MAC_SapHandler((pdDataToMacMessage_t *)msg, msg->ctx_id);
+                    }
+                    break;
+
+                case gPdDataCnf_c:
+                    if (phyLocal[msg->ctx_id].PD_MAC_SapHandler)
+                    {
+                        msg_free = FALSE;
+                        ((pdDataToMacMessage_t *)msg)->msgData.dataCnf.ackData = (uint8_t *)msg + sizeof(pdDataToMacMessage_t);
+
+                        phyLocal[msg->ctx_id].PD_MAC_SapHandler((pdDataToMacMessage_t *)msg, msg->ctx_id);
+                    }
+                    break;
+
+                case gPlmeCcaCnf_c:
+                case gPlmeEdCnf_c:
+                case gPlmeSetCnf_c:
+                case gPlmeTimeoutInd_c:
+                case gPlmeAbortInd_c:
+                    if (phyLocal[msg->ctx_id].PLME_MAC_SapHandler)
+                    {
+                        msg_free = FALSE;
+                        phyLocal[msg->ctx_id].PLME_MAC_SapHandler((plmeToMacMessage_t *)msg, msg->ctx_id);
+                    }
+                    break;
+            }
+
+            if (msg_free)
+            {
+                MSG_Free(msg);
+            }
+        }
+
+        if (!gUseRtos_c)
+        {
+            break;
+        }
+    }
+}
 
 static hal_rpmsg_return_status_t PhyRpmsgRxCallback(void *param, uint8_t *data, uint32_t len)
 {
     (void)param;
+    phyMessageHeader_t *pMsg = (phyMessageHeader_t *)data;
 
-    phyMessageHeader_t *pMsg = (phyMessageHeader_t *)MSG_Alloc(len);
+    if (pMsg->msgType == gPlmeGetReq_c)
+    {
+        if (wait_phy_rsp)
+        {
+            phy_get_rsp = ((macToPlmeMessage_t *)pMsg)->msgData.getReq;
+
+            OSA_EventSet(get_event, 1);
+        }
+        return kStatus_HAL_RL_RELEASE;
+    }
+
+    pMsg = (phyMessageHeader_t *)MSG_Alloc(len);
+    if (!pMsg)
+    {
+        return kStatus_HAL_RL_RELEASE;
+    }
     memcpy(pMsg, data, len);
 
     if (pMsg->ctx_id >= CTX_NO)
@@ -55,77 +172,54 @@ static hal_rpmsg_return_status_t PhyRpmsgRxCallback(void *param, uint8_t *data, 
         pMsg->ctx_id = 0;
     }
 
-    switch(pMsg->msgType)
+    if (OSA_MsgQPut(phy_intf_cb_queue, &pMsg) != KOSA_StatusSuccess)
     {
-        case gPdDataInd_c:
-            ((pdDataToMacMessage_t *)pMsg)->msgData.dataInd.pPsdu = (uint8_t *)pMsg + sizeof(pdDataToMacMessage_t);
-
-            phyLocal[pMsg->ctx_id].PD_MAC_SapHandler((pdDataToMacMessage_t *)pMsg, pMsg->ctx_id);
-            break;
-
-        case gPdDataCnf_c:
-            ((pdDataToMacMessage_t *)pMsg)->msgData.dataCnf.ackData = (uint8_t *)pMsg + sizeof(pdDataToMacMessage_t);
-
-            phyLocal[pMsg->ctx_id].PD_MAC_SapHandler((pdDataToMacMessage_t *)pMsg, pMsg->ctx_id);
-            break;
-
-        case gPlmeCcaCnf_c:
-        case gPlmeEdCnf_c:
-        case gPlmeSetCnf_c:
-        case gPlmeTimeoutInd_c:
-        case gPlmeAbortInd_c:
-            phyLocal[pMsg->ctx_id].PLME_MAC_SapHandler((plmeToMacMessage_t *)pMsg, pMsg->ctx_id);
-            break;
-
-        case gPlmeGetReq_c:
-            MSG_QueueAddTail(&plmeMsgQueue, pMsg);
-            OSA_EventSet(plmeEventHandle, 1);
-            break;
-
-        default:
-            assert(0);
+        MSG_Free(pMsg);
     }
-
     return kStatus_HAL_RL_RELEASE;
 }
 
 void Phy_Init(void)
 {
-    static bool_t init = FALSE;
-
     OSA_InterruptDisable();
 
-    if (init)
+    if (phy_intf_init_done || phy_intf_init_ongoing)
     {
         OSA_InterruptEnable();
-
         return;
     }
 
-    init = TRUE;
+    phy_intf_init_ongoing = TRUE;
 
     OSA_InterruptEnable();
 
     if (HAL_RpmsgInit((hal_rpmsg_handle_t)phyRpmsgHandle, &phyRpmsgConfig) != kStatus_HAL_RpmsgSuccess)
     {
         assert(0);
+        return;
     }
 
     if (HAL_RpmsgInstallRxCallback((hal_rpmsg_handle_t)phyRpmsgHandle, PhyRpmsgRxCallback, NULL) != kStatus_HAL_RpmsgSuccess)
     {
         assert(0);
+        return;
     }
 
-    MSG_QueueInit(&plmeMsgQueue);
-
     /* plmeEventHandle with 0 auto clear to allow the task to run after flags have been set */
-    OSA_EventCreate((osa_event_handle_t)plmeEventHandle, 0);
+    OSA_EventCreate((osa_event_handle_t)get_event, 0);
+
+    OSA_MutexCreate((osa_mutex_handle_t)phy_intf_mutex);
+
+    OSA_MsgQCreate((osa_msgq_handle_t)phy_intf_cb_queue, PHY_INTF_CB_QUEUE_SIZE, sizeof(phyMessageHeader_t *));
+    OSA_TaskCreate((osa_task_handle_t)phy_intf_cb_task, OSA_TASK(phy_intf_cb_task_func), NULL);
 
     /* Configure DTEST signals for debug */
 #if (defined(HWINIT_DEBUG_DTEST) && (HWINIT_DEBUG_DTEST == 1L))
     dtest_init(DTEST0 | DTEST1 | DTEST2 | DTEST3 | DTEST4 | DTEST5 | DTEST6 | DTEST7 | DTEST8 | DTEST9 | DTEST10 | DTEST11 | DTEST12 | DTEST13 );
     dtest_select(0x00); /* Place selftest waveform onto DTEST ports */
 #endif
+
+    phy_intf_init_done = TRUE;
 }
 
 /*! *********************************************************************************
@@ -142,6 +236,11 @@ void Phy_RegisterSapHandlers(PD_MAC_SapHandler_t pPD_MAC_SapHandler,
                              PLME_MAC_SapHandler_t pPLME_MAC_SapHandler,
                              instanceId_t instanceId)
 {
+    if (!phy_intf_init_done)
+    {
+        return;
+    }
+
     if (instanceId >= CTX_NO)
     {
         instanceId = 0;
@@ -162,75 +261,67 @@ void Phy_RegisterSapHandlers(PD_MAC_SapHandler_t pPD_MAC_SapHandler,
 ********************************************************************************** */
 phyStatus_t MAC_PLME_SapHandler(macToPlmeMessage_t *pMsg, instanceId_t phyInstance)
 {
-    phyStatus_t result = gPhySuccess_c;
+    phyStatus_t ret = gPhySuccess_c;
     osa_event_flags_t flags;
 
-    if (phyInstance >= CTX_NO)
+    if (!phy_intf_init_done)
     {
-        phyInstance = 0;
+        return gPhyInvalidParameter_c;
     }
 
-    pMsg->ctx_id = phyInstance;
-
-    /* Bind Event with current task (main_task usually) in case RPMSG response is faster than
-     * OSA_EventWait in the loop below.
-     */
-    if (pMsg->msgType == gPlmeGetReq_c)
-    {
-        OSA_EventWait(plmeEventHandle, 1, 1, osaWaitNone_c, &flags);
-    }
-
-#if defined(HDI_MODE) && (HDI_MODE == 1)
-    if ((pMsg->msgType == gPlmeSetReq_c) && (pMsg->msgData.setReq.PibAttribute == gPhyPibCurrentChannel_c))
-    {
-        if (pMsg->msgData.setReq.PibAttribute == gPhyPibCurrentChannel_c)
-        {
-            HDI_SendChannelSwitchCmd((uint32_t)pMsg->msgData.setReq.PibAttributeValue);
-        }
-    }
-#endif
-
+    OSA_MutexLock(phy_intf_mutex, osaWaitForever_c);
     PLATFORM_RemoteActiveReq();
 
-    if (HAL_RpmsgSend((hal_rpmsg_handle_t)phyRpmsgHandle, (uint8_t *)pMsg, sizeof(macToPlmeMessage_t)) != kStatus_HAL_RpmsgSuccess)
+    do
     {
-        assert(0);
-    }
-
-    if (pMsg->msgType == gPlmeGetReq_c)
-    {
-        macToPlmeMessage_t *response;
-        uint32_t dt = osaWaitForever_c;
-
-        if (!OSA_TaskGetCurrentHandle())
+        if (phyInstance >= CTX_NO)
         {
-            /* Current task is valid in OSA_ProcessTasks().
-               There is no check in OSA_EventWait() that current task is valid.
-               For osaWaitNone_c, current task is not used */
-            dt = osaWaitNone_c;
+            phyInstance = 0;
         }
 
-        /* Wait until NBU delivers PIB value over RPMSG */
-        while (OSA_EventWait(plmeEventHandle, 1, 1, dt, &flags) != KOSA_StatusSuccess)
-        {}
+        pMsg->ctx_id = phyInstance;
 
-        /* Clear event as auto clear is not enabled */
-        OSA_EventClear(plmeEventHandle, 1);
-
-        response = MSG_QueueRemoveHead(&plmeMsgQueue);
-
-        if (response == NULL || response->msgType != pMsg->msgType)
+        /* Bind Event with current task (main_task usually) in case RPMSG response is faster than
+         * OSA_EventWait in the loop below.
+         */
+        if (pMsg->msgType == gPlmeGetReq_c)
         {
-            assert(0);
+            phy_get_rsp.PibAttributeValue = 0;
+
+            OSA_EventWait(get_event, 1, 1, osaWaitNone_c, &flags);
+            wait_phy_rsp = TRUE;
         }
 
-        pMsg->msgData.getReq.PibAttributeValue = response->msgData.getReq.PibAttributeValue;
-        MSG_Free(response);
-    }
+#if defined(HDI_MODE) && (HDI_MODE == 1)
+        if ((pMsg->msgType == gPlmeSetReq_c) && (pMsg->msgData.setReq.PibAttribute == gPhyPibCurrentChannel_c))
+        {
+            if (pMsg->msgData.setReq.PibAttribute == gPhyPibCurrentChannel_c)
+            {
+                HDI_SendChannelSwitchCmd((uint32_t)pMsg->msgData.setReq.PibAttributeValue);
+            }
+        }
+#endif
 
+        if (HAL_RpmsgSend((hal_rpmsg_handle_t)phyRpmsgHandle, (uint8_t *)pMsg, sizeof(macToPlmeMessage_t)) != kStatus_HAL_RpmsgSuccess)
+        {
+            ret = gPhyBusy_c;
+            break;
+        }
+
+        if (pMsg->msgType == gPlmeGetReq_c)
+        {
+            wait_response();
+            wait_phy_rsp = FALSE;
+
+            pMsg->msgData.getReq.PibAttributeValue = phy_get_rsp.PibAttributeValue;
+        }
+    } while(0);
+
+    wait_phy_rsp = FALSE;
     PLATFORM_RemoteActiveRel();
+    OSA_MutexUnlock(phy_intf_mutex);
 
-    return result;
+    return ret;
 }
 
 /*! *********************************************************************************
@@ -244,45 +335,61 @@ phyStatus_t MAC_PLME_SapHandler(macToPlmeMessage_t *pMsg, instanceId_t phyInstan
 ********************************************************************************** */
 phyStatus_t MAC_PD_SapHandler(macToPdDataMessage_t *pMsg, instanceId_t phyInstance)
 {
-    uint32_t len;
-    phyStatus_t result = gPhySuccess_c;
+    uint32_t len = sizeof(macToPdDataMessage_t);
+    phyStatus_t ret = gPhySuccess_c;
 
-    if (phyInstance >= CTX_NO)
+    if (!phy_intf_init_done)
     {
-        phyInstance = 0;
+        return gPhyInvalidParameter_c;
     }
 
-    pMsg->ctx_id = phyInstance;
-
-    len = sizeof(macToPdDataMessage_t);
-
-    if (pMsg->msgType == gPdDataReq_c)
-    {
-        len += pMsg->msgData.dataReq.psduLength;
-    }
-
+    OSA_MutexLock(phy_intf_mutex, osaWaitForever_c);
     PLATFORM_RemoteActiveReq();
 
-    if (HAL_RpmsgSend((hal_rpmsg_handle_t)phyRpmsgHandle, (uint8_t *)pMsg, len) != kStatus_HAL_RpmsgSuccess)
+    do
     {
-         assert(0);
-    }
+        if (phyInstance >= CTX_NO)
+        {
+            phyInstance = 0;
+        }
+
+        pMsg->ctx_id = phyInstance;
+
+        if (pMsg->msgType == gPdDataReq_c)
+        {
+            len += pMsg->msgData.dataReq.psduLength;
+        }
+
+        if (HAL_RpmsgSend((hal_rpmsg_handle_t)phyRpmsgHandle, (uint8_t *)pMsg, len) != kStatus_HAL_RpmsgSuccess)
+        {
+             ret = gPhyBusy_c;
+             break;
+        }
+    } while (0);
 
     PLATFORM_RemoteActiveRel();
+    OSA_MutexUnlock(phy_intf_mutex);
 
-    return result;
+    return ret;
 }
 
 uint8_t PHY_get_ctx()
 {
+    uint8_t cnt = PHY_GET_CTX_CNT;
+    phyStatus_t ret;
     macToPlmeMessage_t msg;
 
     msg.msgType = gPlmeGetReq_c;
     msg.msgData.getReq.PibAttribute = gPhyGetCtxId;
     msg.msgData.getReq.PibAttributeValue = (uint64_t)(-1);
 
-    MAC_PLME_SapHandler(&msg, 0);
+    /* wait for NBU to be ready */
+    do
+    {
+        ret = MAC_PLME_SapHandler(&msg, 0);
+    } while ((ret != gPhySuccess_c) && (cnt--));
 
+    assert(ret == gPhySuccess_c);
     return (uint8_t)msg.msgData.getReq.PibAttributeValue;
 }
 

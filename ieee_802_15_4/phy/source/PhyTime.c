20ffcfb91bba38b7825b67a58d223a1102377956
George Stefan <george.stefan@nxp.com>
2025-07-17 12:06:40 +0300

    [MCZB-2862] PHY: update copyright
    Signed-off-by: George Stefan <george.stefan@nxp.com>

--- ieee_802_15_4/phy/source/PhyTime.c
+++ ieee_802_15_4/phy/source/PhyTime.c
@@ -1,6 +1,6 @@
 /*! *********************************************************************************
 * Copyright (c) 2015, Freescale Semiconductor, Inc.
-* Copyright 2018-2024 NXP
+* Copyright 2018-2025 NXP
 * All rights reserved.
 *
 * \file

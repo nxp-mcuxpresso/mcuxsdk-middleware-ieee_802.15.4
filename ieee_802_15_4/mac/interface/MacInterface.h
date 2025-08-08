22c21ec6a9c254aea1c82b0eeaadfc9c89d46022
George Stefan <george.stefan@nxp.com>
2025-08-08 10:11:02 +0300

    [MCZB-2973][MCZB-2147] MAC: one instances
    - memory configuration doesn't allow two MAC instances (stack corruption)
    - extended POLL state machine (POLL accelerator) is not ready on the main core side
    
    Signed-off-by: George Stefan <george.stefan@nxp.com>

--- ieee_802_15_4/mac/interface/MacInterface.h
+++ ieee_802_15_4/mac/interface/MacInterface.h
@@ -79,11 +79,6 @@ enum
 /*!
  *  The number of MAC instances used
  */
-#ifdef PHY_LP_TMR
-#undef gMacInstancesCnt_c
-#define gMacInstancesCnt_c 2
-#endif
-
 #ifndef gMacInstancesCnt_c
 #define gMacInstancesCnt_c  1
 #endif

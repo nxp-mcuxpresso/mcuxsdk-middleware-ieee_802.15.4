f2ab03fe449249a38aaddbb78c8d019933b6b527
George Stefan <george.stefan@nxp.com>
2025-12-23 12:36:49 +0200

    [MCZB-3292] mcxw72: MAC: two instances
    One is used by the extended POLL state machine (POLL accelerator)
    
    Signed-off-by: George Stefan <george.stefan@nxp.com>

--- ieee_802_15_4/mac/interface/MacInterface.h
+++ ieee_802_15_4/mac/interface/MacInterface.h
@@ -1,6 +1,6 @@
 /*! *********************************************************************************
 * Copyright (c) 2015, Freescale Semiconductor, Inc.
-* Copyright 2016-2025 NXP
+* Copyright 2016-2026 NXP
 * All rights reserved.
 *
 * \file
@@ -79,6 +79,11 @@ enum
 /*!
  *  The number of MAC instances used
  */
+#if defined(PHY_LP_TMR) && defined(MCXW727C_cm33_core1_SERIES)
+#undef gMacInstancesCnt_c
+#define gMacInstancesCnt_c 2
+#endif
+
 #ifndef gMacInstancesCnt_c
 #define gMacInstancesCnt_c  1
 #endif

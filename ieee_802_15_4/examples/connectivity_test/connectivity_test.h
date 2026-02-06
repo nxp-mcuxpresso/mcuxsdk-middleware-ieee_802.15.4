7e710ab026dbb30338b781dcac21249ef0cb00ea
Radu Alexe <radu.alexe@nxp.com>
2026-02-06 12:32:40 +0200

    [MCZB-3151] conn_test/smac: correctly update the copyright

--- ieee_802_15_4/examples/connectivity_test/connectivity_test.h
+++ ieee_802_15_4/examples/connectivity_test/connectivity_test.h
@@ -1,6 +1,6 @@
 /*! *********************************************************************************
 * Copyright (c) 2015, Freescale Semiconductor, Inc.
-* Copyright 2016-2024 NXP
+* Copyright 2016-2024,2026 NXP
 * All rights reserved.
 *
 * \file
--- smac/source/SMAC.c
+++ smac/source/SMAC.c
@@ -1,6 +1,6 @@
 /*! *********************************************************************************
 * Copyright (c) 2014 - 2025, Freescale Semiconductor, Inc.
-* Copyright 2016-2026 NXP
+* Copyright 2016-2024,2026 NXP
 * All rights reserved.
 *
 * \file

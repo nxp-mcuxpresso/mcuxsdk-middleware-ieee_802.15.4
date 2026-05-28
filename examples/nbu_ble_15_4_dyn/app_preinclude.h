67579eb918a8f1e02dd052f9a9cf6dba2ee700c6
Radu Alexe <radu.alexe@nxp.com>
2026-05-28 16:54:08 +0300

    [ZEP-5996] nbu_dyn: Set HEAP size to be 1024
    This settings mimics ble_nbu settings since the limit is when we are
    building nbu_dyn_full (which integrates the full ble lib).

--- examples/nbu_ble_15_4_dyn/app_preinclude.h
+++ examples/nbu_ble_15_4_dyn/app_preinclude.h
@@ -55,11 +55,19 @@
 #define gAppMaxTxPowerDbm_c 10
 
 #if defined(CPU_MCXW727CMFTA_cm33_core1)
+
 /* Using BLE project nbu_ble heap settings
    Extend Heap usage beyond the size defined by MinimalHeapSize_c*/
 #define MinimalHeapSize_c        (uint32_t)8192
 #define gMemManagerLightExtendHeapAreaUsage 0
+
+#elif defined(KW43B43ZC7_NBU_SERIES)
+
+#define MinimalHeapSize_c        (uint32_t)1024
+#define gMemManagerLightExtendHeapAreaUsage 0
+
 #else
+
 #define gMemManagerLightExtendHeapAreaUsage 1
 #endif
 
